d347210c0767f96e9644afbe15ef517f7ec4a99d
Sahil_Sinha <sahil.sinha@juspay.in>
2019-09-10 00:28:09 +0530

    Added minor animation, implemeted background image changer, optimized load time (imageSize Fix)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'MegabrandProducts';
+  photo : string;
+  imageArray : string[];
+  index : number;
+  timer : any;
+
+  constructor() {
+    this.imageArray = [
+        "../assets/homepage.jpg"
+    ,   "../assets/homepage2.jpg"
+    ];
+    this.index = 0;
+    this.photo = this.imageArray[0];
+
+    this.timer = setInterval( () => {
+        this.index = this.index + 1;
+        if (this.imageArray === undefined) 
+            console.log("imageArrayNotPresent");
+        else if(this.index == this.imageArray.length) 
+            this.index = 0;
+        this.photo = this.imageArray[this.index];
+    }, 10000);
+  }
+
 }
